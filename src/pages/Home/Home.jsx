b1036db924a6eab8231822f32e56c4066c4e8d5c
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-11-14 22:07:59 +0900

    Sub Swiper 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import MainSwiper from '../../components/Swiper/MainSwiper';
+import SubSwiper from '../../components/Swiper/SubSwiper';
 
 const Home = () => {
 	return (
 		<>
 			<MainSwiper />
+			<SubSwiper dataTitle="NEW ARRIVAL" />
+			<SubSwiper dataTitle="OUTER" />
+			<SubSwiper dataTitle="TOP" />
+			<SubSwiper dataTitle="BOTTOM" />
+			<SubSwiper dataTitle="ACC" />
 		</>
 	);
 };
