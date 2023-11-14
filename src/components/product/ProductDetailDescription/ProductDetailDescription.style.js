bfc44a5f85c6d7b34f3cff349427944fcff1adf4
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-15 00:36:30 +0900

    feat: 상세 설명 부분 구현
    2023.11.15

---
+++ src/components/product/ProductDetailDescription/ProductDetailDescription.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const DescriptionSection = styled.section`
+	width: 100%;
+	padding: 60px;
+
+	& p {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+
+	& ul {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+`;
+
+export const ImgWrap = styled.div`
+	margin-bottom: 60px;
+`;
