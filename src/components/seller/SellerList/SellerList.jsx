a7d2d79d684ac2a1d897dc66de0c743774d40685
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-15 12:53:36 +0900

    feat: 판매 물품 리스트 UI 구현하기
    2023.11.15

--- src/components/seller/SellerList/SellerList.jsx
+++ src/components/seller/SellerList/SellerList.jsx
@@ -1,5 +1,159 @@
+import {
+	Button,
+	ButtonsWrap,
+	DiscountWrap,
+	ImgWrap,
+	ItemInfoWrap,
+	ItemTitle,
+	PriceAndButtonsWrap,
+	SellerListContainer,
+	SellerListItem,
+	StockWrap,
+} from './SellerList.style';
+
 const SellerList = () => {
-	return <div>SellerList</div>;
+	return (
+		<SellerListContainer>
+			<ul>
+				<SellerListItem>
+					<ImgWrap>
+						<img src="/assets/main1.jpg" alt="" />
+					</ImgWrap>
+					<ItemInfoWrap>
+						<ItemTitle>골드 코인 장식 스웨이드 페니로퍼</ItemTitle>
+						<div>카테고리 : 슈즈</div>
+						<DiscountWrap>
+							<label htmlFor="">할인율</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>%</span>
+						</DiscountWrap>
+						<StockWrap>
+							<label htmlFor="">재고</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>개</span>
+						</StockWrap>
+					</ItemInfoWrap>
+					<PriceAndButtonsWrap>
+						<div>45,500원</div>
+						<ButtonsWrap>
+							<Button>삭제</Button>
+							<Button>저장</Button>
+						</ButtonsWrap>
+					</PriceAndButtonsWrap>
+				</SellerListItem>
+				<SellerListItem>
+					<ImgWrap>
+						<img src="/assets/main1.jpg" alt="" />
+					</ImgWrap>
+					<ItemInfoWrap>
+						<ItemTitle>골드 코인 장식 스웨이드 페니로퍼</ItemTitle>
+						<div>카테고리 : 슈즈</div>
+						<DiscountWrap>
+							<label htmlFor="">할인율</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>%</span>
+						</DiscountWrap>
+						<StockWrap>
+							<label htmlFor="">재고</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>개</span>
+						</StockWrap>
+					</ItemInfoWrap>
+					<PriceAndButtonsWrap>
+						<div>45,500원</div>
+						<ButtonsWrap>
+							<Button>삭제</Button>
+							<Button>저장</Button>
+						</ButtonsWrap>
+					</PriceAndButtonsWrap>
+				</SellerListItem>
+				<SellerListItem>
+					<ImgWrap>
+						<img src="/assets/main1.jpg" alt="" />
+					</ImgWrap>
+					<ItemInfoWrap>
+						<ItemTitle>골드 코인 장식 스웨이드 페니로퍼</ItemTitle>
+						<div>카테고리 : 슈즈</div>
+						<DiscountWrap>
+							<label htmlFor="">할인율</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>%</span>
+						</DiscountWrap>
+						<StockWrap>
+							<label htmlFor="">재고</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>개</span>
+						</StockWrap>
+					</ItemInfoWrap>
+					<PriceAndButtonsWrap>
+						<div>45,500원</div>
+						<ButtonsWrap>
+							<Button>삭제</Button>
+							<Button>저장</Button>
+						</ButtonsWrap>
+					</PriceAndButtonsWrap>
+				</SellerListItem>
+				<SellerListItem>
+					<ImgWrap>
+						<img src="/assets/main1.jpg" alt="" />
+					</ImgWrap>
+					<ItemInfoWrap>
+						<ItemTitle>골드 코인 장식 스웨이드 페니로퍼</ItemTitle>
+						<div>카테고리 : 슈즈</div>
+						<DiscountWrap>
+							<label htmlFor="">할인율</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>%</span>
+						</DiscountWrap>
+						<StockWrap>
+							<label htmlFor="">재고</label>
+							<div>
+								<button>-</button>
+								<input type="text" value="1" />
+								<button>+</button>
+							</div>
+							<span>개</span>
+						</StockWrap>
+					</ItemInfoWrap>
+					<PriceAndButtonsWrap>
+						<div>45,500원</div>
+						<ButtonsWrap>
+							<Button>삭제</Button>
+							<Button>저장</Button>
+						</ButtonsWrap>
+					</PriceAndButtonsWrap>
+				</SellerListItem>
+			</ul>
+		</SellerListContainer>
+	);
 };
 
 export default SellerList;
