1de823aa4fdaa6f6fb1376b9b5d8e1457a0cad75
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-20 16:08:00 +0900

    feat: api코드 추가하고 일부 Mock 데이터만 추가하고
    2023.11.20

---
+++ src/api/ApiClient.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import localToken from './LocalToken';
+
+class ApiClient {
+	constructor(baseURL, headers, config) {
+		this.baseURL = baseURL;
+		this.headers = headers;
+		this.api = axios.create({ baseURL, headers });
+	}
+
+	get(endpoint) {
+		return this.request('get', endpoint);
+	}
+
+	post(endpoint, body) {
+		return this.request('post', endpoint, body);
+	}
+
+	put(endpoint, body) {
+		return this.request('put', endpoint, body);
+	}
+
+	delete(endpoint) {
+		return this.request('delete', endpoint);
+	}
+
+	request(method, url, data, config) {
+		return this.api
+			.request({
+				method,
+				url,
+				data: method === 'post' || method === 'put' ? data : undefined,
+				headers: {
+					...this.headers,
+					'Authorization': `Bearer ${localToken.get()}`,
+					'Content-Type': 'application/json',
+				},
+				...config,
+			})
+			.then((res) => res.data)
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+}
+
+export default ApiClient;
