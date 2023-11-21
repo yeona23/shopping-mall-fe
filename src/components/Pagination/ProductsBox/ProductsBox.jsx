f0040db1516949c33dfd4ff370ed512c86f602e3
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-11-21 10:43:05 +0900

    test: products페이지 mockdata연결

--- src/components/Pagination/ProductsBox/ProductsBox.jsx
+++ src/components/Pagination/ProductsBox/ProductsBox.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import { ItemBox, ProductList } from './ProductsBox.style';
 import PageButton from '../PageButton';
 
-const generateImgUrl = (index) => {
-	const maxIndex = 10;
+const generateImgUrl = (dataTitle, index) => {
+	const maxIndex = 4;
 	const actualIndex = index <= maxIndex ? index : (index % maxIndex) + 1;
-	return `/assets/main${actualIndex}.jpg`;
+	return dataTitle === 'OUTER'
+		? `/assets/jacket_${actualIndex}/jacket_${actualIndex}_thumb.jpg`
+		: `/assets/knit_${actualIndex}/knit_${actualIndex}_thumb.jpg`;
 };
 
-const ProductItem = ({ index, price, itemTitle }) => (
+const ProductItem = ({ dataTitle, index, price, itemTitle }) => (
 	<ItemBox>
 		<div>
-			<img src={generateImgUrl(index)} alt=" " />
+			<img src={generateImgUrl(dataTitle, index)} alt=" " />
 		</div>
 		<p>{itemTitle}</p>
 		<span>{price}</span>
 	</ItemBox>
 );
 
-const ProductsBox = () => {
+const ProductsBox = ({ dataTitle }) => {
 	const items = Array.from({ length: 16 }, (_, index) => ({
 		id: index + 1,
 		index: index + 1,
@@ -29,7 +31,7 @@ const ProductsBox = () => {
 		<>
 			<ProductList>
 				{items.map((item) => (
-					<ProductItem key={item.id} {...item} />
+					<ProductItem key={item.id} dataTitle={dataTitle} {...item} />
 				))}
 			</ProductList>
 			<PageButton />
