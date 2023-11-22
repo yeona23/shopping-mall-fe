a52752eaa58aee393fb52cb762851a10a2ff6071
DataCodeLiteracy <dataliteracy@icloud.com>
2023-11-23 01:21:51 +0900

    fix: rebase 후에 중복되는 함수 제거
    2023.11.23

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -96,22 +96,6 @@ const Login = () => {
 	const namePasswordInputIsInValid =
 		!passwordIsValid && textIsTouched && isSignInClicked;
 
-	const loginUserHandler = async (e) => {
-		e.preventDefault();
-
-		try {
-			const response = await loginUser(inputValue);
-
-			console.log(response);
-
-			const { access_token } = response;
-
-			localToken.save(access_token);
-		} catch (error) {
-			console.error(error.message);
-		}
-	};
-
 	return (
 		<LoginWrapper>
 			<AccountDiv>ACCOUNT</AccountDiv>
