bea50aae9f172161f33562c3edd44bbd026ad91b
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-11-23 11:16:11 +0900

    feat: 장바구니 물품 내역 api로 첫 로딩시 불러오기

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,9 +1,18 @@
 import ApiClient from './ApiClient';
 
+const UPDATE = '/update';
 const REGISTER = '/register';
 const GET_CART = '/get';
 const BASE_URL = 'http://3.34.122.57:8080';
 
+// mock
+const cartMockApi = new ApiClient('/data');
+
+export const getMockCart = async () => {
+	return await cartMockApi.get('cart.json');
+};
+
+// real api
 const cartApi = new ApiClient(BASE_URL + '/api/cart');
 
 export const getCartProducts = async () => {
@@ -15,5 +24,13 @@ export const registerCartProduct = async (endpoint, body) => {
 		quantity: body.count,
 		color: body.color,
 		size: body.size,
+	});
+};
+
+export const updateCartProduct = async (endpoint, body) => {
+	return await cartApi.put(UPDATE + `/${endpoint}`, {
+		color: body.color,
+		size: body.size,
+		cartQuantity: Number(body.quantity),
 	});
 };
