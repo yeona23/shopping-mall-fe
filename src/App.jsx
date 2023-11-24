934d2c07530204cd5c93f3d2dfee9f1bfae52854
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-11-24 17:19:28 +0900

    fix: 라우트 이동 시, 스크롤값 유지문제 해결
    커스텀 훅을 만들어 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
+import useScrollToTop from './hooks/useScrollToTop';
 
 function App() {
+	useScrollToTop();
+
 	return (
 		<>
 			<Nav />
